1afe70f92aaed699cd0570414dbd19092fca5018
HP <HP@DESKTOP-B0OK1FL>
2021-01-14 14:54:25 +0700

    Add project files.

---
+++ book class/Class.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace book_class
+{
+    class Class
+    {
+        private string title;
+        private string author;
+        private bool borrowed;
+        private int pages;
+        public static int numberofbooks = 0;
+
+        public Book(string title, string author, int pages)
+        {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.borrowed = false;
+            numberofbooks = numberofbooks + 1;
+        }
+
+        public string Gettitle()
+        {
+            return title;
+        }
+
+        public string Getauthor()
+        {
+            return author;
+        }
+
+        public int Getpages()
+        {
+            return pages;
+        }
+
+        public bool Isborrowed()
+        {
+            return borrowed;
+        }
+
+        public void Borrowbook()
+        {
+            if (borrowed == false)
+            {
+                borrowed = true;
+            }
+            else
+            {
+                Console.WriteLine(title + " is not available");
+            }
+
+        }
+
+
+        public void Returnbook()
+        {
+            if (borrowed == true)
+            {
+                borrowed = false;
+            }
+            else
+            {
+                Console.WriteLine(title + " cannot be returned because it has not been borrowed");
+            }
+        }
+    }
---
+++ book class/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace book_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        }
+    }
+}
